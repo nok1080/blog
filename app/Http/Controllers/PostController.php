83bfaf546e6500b8e76231d75b020fc0400e41f9
nok1080 <nok1080@icloud.com>
2023-02-28 20:11:43 +0900

    8-3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,9 +8,16 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
-{
+    
+ {
    return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
 
-}
+ }
 
+
+      public function show(Post $post)
+     {
+          return view('posts/show')->with(['post' => $post]);
+     }
+     
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,14 +5,15 @@
         <title>Blog</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+  
     </head>
     <body>
         <h1>Blog Name</h1>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
-                    <p class='body'>{{ $post->body }}</p>
+                        <a herf="/posts/{{ $post->id }}"><h2 class='title'>{{ $post->title }}</h2></a>
+                        <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
         </div>
